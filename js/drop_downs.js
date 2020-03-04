a1c7b9f51c6096fa38e14815fbc0244e0a803ae6
Kai Banto Eckert <y.banto@outlook.com>
2020-03-04 17:17:23 +0100

    Added js file for drop down button

---
+++ js/drop_downs.js
@@ -0,0 +1,8 @@
+function drop_down() {
+  var x = document.getElementById("basic_workflow_list");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
